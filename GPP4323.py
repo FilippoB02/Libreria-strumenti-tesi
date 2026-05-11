3a3d6595b886ede87eb5d944fa798f5856c63ea2
FilippoB02 <filbs29@gmail.com>
2026-05-11 19:40:11 +0200

    Prima versione GPP

--- GPP4323.py
+++ GPP4323.py
@@ -3,84 +3,207 @@ import time
 
 class GPP4323:
     """
-    Libreria per controllare GW Instek GPP-4323 via SCPI tramite VISA.
-    Compatibile con USB-TMC, USB-CDC (ASRL), RS232 e LAN (SOCKET).
+    GW INSTEK GPP-4323 programmabre DC power supply
+    
+    High-level Python interface for controlling a GPP-4323 power supply
+    using PyVISA library and SCPI commands.
+    
+    Need to download GPP driver setup from gwinstek website for USB connection
+
+    Implements common SCPI commands for:
+    - Identification
+    - Reset / clear
+    - Current and Voltage selection
+    - Channel selection
+    
+    Examples
+    --------
+    >>> from fastruments.Keysight53220A import Keysight53220A
+    >>> freq = Keysight53220A('USB0::0x0957::0x1807::MY63260252::INSTR')
+    [53220A] IDN: Agilent Technologies,53220A,MY63260252,03.02-1924.2831-3.15-4.16-127-159-35
+    [53220A] Connection successful.
+    
+    
+    
+    >>> freq.close()
+    [53220A] Connection closed.
+    
+    FINISCI ESEMPIO
+    
     """
 
-    def __init__(self, resource, timeout=2000):
-        self.rm = pyvisa.ResourceManager()
-        self.inst = self.rm.open_resource(resource)
-        self.inst.timeout = timeout
-        self.inst.read_termination = '\n'
-        self.inst.write_termination = '\n'
+    def __init__(self, resource, verbose: bool = True) -> None:
+        """
+        Initialize communication with GPP-4323 power supply.
+        """
+        
+        self.resource = resource
+        self.verbose = verbose
+        self.connect()
+        self.delay = 0.3
+        
+    # ---------------------------------------------------------
+    # General communication and status
+    # ---------------------------------------------------------
+        
+    def connect(self) -> None:
+        """
+        Establish the VISA connection and verify communication.
+        """
+        try:
+            rm = pyvisa.ResourceManager()
+            self.inst = rm.open_resource(self.resource)
+            self.inst.read_termination = '\n'
+            self.inst.write_termination = '\n'
+        except Exception as e:
+            raise ConnectionError(f"[GPP4323][ERROR] Failed to connect: {e}")
 
-    # ------------------------------------------------------------
-    # Informazioni di base
-    # ------------------------------------------------------------
-    def idn(self):
-        """Ritorna l'identificazione dello strumento."""
-        return self.inst.query("*IDN?")
+        try:
+            self.idn()
+            if self.verbose:
+                print("[53220A] Connection successful.")
+        except Exception as e:
+            raise RuntimeError(f"[GPP4323][ERROR] IDN query failed: {e}")
 
-    def reset(self):
-        """Reset dello strumento."""
+    def idn(self) -> str:
+        """
+        Query the identification string.
+
+        Returns
+        -------
+        str
+            Full identification string.
+        """
+        idn = self.inst.query("*IDN?")
+        if self.verbose:
+            print(f"[GPP4323] IDN: {idn}")
+        return idn
+
+    def reset(self) -> None:
+        """Reset the instrument to factory defaults."""
         self.inst.write("*RST")
+        if self.verbose:
+            print("[GPP4323] Instrument reset.")
 
-    def clear(self):
-        """Reset degli status/errori."""
+    def clear(self) -> None:
+        """Clear status and error registers."""
         self.inst.write("*CLS")
+        if self.verbose:
+            print("[GPP4323] Status registers cleared.")
+        
+    def close(self) -> None:
+        """
+        Close the VISA connection to the instrument.
+
+        Notes
+        -----
+        Should always be called before program termination to release the USB resource.
+
+        Raises
+        ------
+        RuntimeError
+            If the resource cannot be cleanly closed.
+        """
+        try:
+            self.inst.close()
+            if self.verbose:
+                print("[GPP4323] Connection closed.")
+        except Exception as e:
+            raise RuntimeError(f"[GPP4323][ERROR] Failed to close: {e}")
+            
+    """
+    METTI APPOSTO I COMMENTI
+    """
 
     # ------------------------------------------------------------
-    # Selezione canale
+    # Output channel configuration
     # ------------------------------------------------------------
-    def select_channel(self, ch: int):
-        """Seleziona il canale (1..4)."""
-        self.inst.write(f"INST:NSEL {ch}")
-
-    # ------------------------------------------------------------
-    # Impostazioni di uscita
-    # ------------------------------------------------------------
+    
     def set_voltage(self, ch: int, volts: float):
         """Imposta la tensione di un canale."""
         self.inst.write(f"SOUR{ch}:VOLT {volts}")
+        if self.verbose:
+            print("[GPP4323] CH{ch}: Voltage set to {volts} V.")
 
     def set_current(self, ch: int, amps: float):
         """Imposta la corrente di un canale."""
         self.inst.write(f"SOUR{ch}:CURR {amps}")
+        if self.verbose:
+            print("[GPP4323] CH{ch}: current set to {amps} A.")
+        
+    """
+    prova iset e vset e a cosa servono
+    FAI LA FUNZIONE DI CHECK
+    SCRIVI I COMMENTI
+    
+    funzione per la resistenza
+    SOURce[1|2]:RESistor < NRf >
+    serve usare mode CR ovvero resistenza costsnte, si comorta come carico
+    """
+        
+    # ------------------------------------------------------------
+    # Turning channels on and off
+    # ------------------------------------------------------------
 
     def output_on(self, ch: int):
         """Abilita l'uscita di un canale."""
         self.inst.write(f"OUTP{ch} ON")
-        
-    def output_on_wait(self, ch: int, delay=0.2):
-        self.inst.write(f"OUTP{ch} ON")
-        time.sleep(delay)
+        time.sleep(self.delay)
 
     def output_off(self, ch: int):
         """Disabilita l'uscita di un canale."""
         self.inst.write(f"OUTP{ch} OFF")
+        time.sleep(self.delay)
+        
+    def output_on_all(self):
+        self.inst.write("ALLOUTON")
+        time.sleep(self.delay)
+        
+    def output_off_all(self):
+        self.inst.write("ALLOUTOFF")
+        time.sleep(self.delay)
+        
+    """
+    SCRIVI I COMMENTI E I VERBOSE
+    """
 
     # ------------------------------------------------------------
-    # Lettura misure
+    # Getting set parameters
     # ------------------------------------------------------------
-    def measure_voltage(self, ch: int):
-        """Legge la tensione del canale."""
-        return float(self.inst.query(f"MEAS{ch}:VOLT?"))
+    
+    def measure_param(self, ch: int):
+        volt = self.inst.query(f"MEAS{ch}:VOLT?")
+        curr = self.inst.query(f"MEAS{ch}:CURR?")
+        power = self.inst.query(f"MEAS{ch}:POWE?")
+        if self.verbose:
+            print(f"[GPP4323] CH{ch}: Measured values imposed by the instrument:")
+            print(f"Voltage: {volt} V")
+            print(f"Current: {curr} A")
+            print(f"Power: {power} W")
+        
 
-    def measure_current(self, ch: int):
-        """Legge la corrente del canale."""
-        return float(self.inst.query(f"MEAS{ch}:CURR?"))
-
+    """
+    devo fare anche quello che fa get del valore impostato
+    o solo quello del valore effettivamnte erogato?
+    SCRIVI I COMMENTI
+    """
+    
     # ------------------------------------------------------------
-    # Funzioni varie
+    # Setting series and parallel channel 1 - 2
     # ------------------------------------------------------------
-    def beep(self):
-        """Esegue un beep (utile per debug)."""
-        self.inst.write("BEEP")
-
-    # ------------------------------------------------------------
-    # Chiusura
-    # ------------------------------------------------------------
-    def close(self):
-        """Chiude la connessione VISA."""
-        self.inst.close()
-        self.rm.close()
+    
+    def series(self, state: str):
+        self.inst.write(f"OUTP:SER {state}")
+        time.sleep(self.delay)
+        
+    def parallel(self, state: str):
+        self.inst.write(f"OUTP:PAR {state}")
+        time.sleep(self.delay)
+    
+    """
+    SCRIVI I COMMENTI
+    """
+    
+    """
+    SCRIVI IL MAIN
+    """
--- Instruction GPP4323.py
+++ Instruction GPP4323.py
@@ -8,19 +8,57 @@ rm = pyvisa.ResourceManager()
 print(rm.list_resources())
 """
 
-gen = GPP4323("ASRL4::INSTR")  # PROVA A TOGLIERE IL PUNTO
+gen = GPP4323("ASRL3::INSTR")
 
-print(gen.idn())
 
-gen.set_voltage(1, 4.1)
-gen.set_current(1, 0.5)
+
+gen.idn()
+gen.reset()
+gen.clear()
+
+
+gen.set_voltage(1, 1)
+gen.set_current(1, 1)
+gen.set_voltage(2, 2)
+gen.set_current(2, 5)
+gen.set_voltage(3, 3)
+gen.set_current(3, 5)
+gen.set_voltage(4, 4)
+gen.set_current(4, 5)
+
+# resistenza
+
 gen.output_on(1)
+gen.output_on(2)
+gen.output_on(3)
+gen.output_on(4)
+
 time.sleep(0.5)
 
-v = gen.measure_voltage(1)
-i = gen.measure_current(1)
-
-print("Misure:", v, "V", i, "A")
-
 gen.output_off(1)
+gen.output_off(2)
+gen.output_off(3)
+gen.output_off(4)
+
+gen.output_on_all()
+
+gen.measure_param(1)
+gen.measure_param(2)
+gen.measure_param(3)
+gen.measure_param(4)
+
+gen.output_off_all()
+
+"""
+gen.series('ON')
+gen.series('OFF')
+gen.parallel('ON')
+gen.parallel('OFF')
+
+gen.parallel('ON')
+gen.series('ON')
+gen.series('OFF')
+"""
+
 gen.close()
+
